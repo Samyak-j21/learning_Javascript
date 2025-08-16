f1b237024da9a6c7653b8cc229687f7b6bcf326a
Samyak-j21 <samyakhain123@gmail.com>
2025-08-16 14:07:16 +0530

    convert products into classes

--- Amazon-clone/data/products.js
+++ Amazon-clone/data/products.js
@@ -1,3 +1,5 @@
+import { formatCurrency } from '../scripts/utils/money.js'
+
 
 export function getProduct(productId) {
   let matchingProduct;
@@ -9,6 +11,30 @@ export function getProduct(productId) {
   return matchingProduct;
 }
 
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceRupees;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceRupees = productDetails.priceRupees;
+  }
+
+  getStarsUrl() {
+    return ` images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return `₹${formatCurrency(this.priceRupees)}`
+  }
+}
 
 export const products = [
   {
@@ -669,4 +695,7 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  return new Product(productDetails);
+});
+
